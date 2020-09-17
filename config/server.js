bb1244d1c6a85742a3331f312f919556c35aa741
fernando espinosa <fernando.espinosa@payclip.com>
2020-09-16 20:44:29 -0500

    initial strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c176fbc7e7094bda2247bfe34533be9e'),
+    },
+  },
+});
